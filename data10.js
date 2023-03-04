32e478d50a0083538f5e0cd95b07f7ae08436c01
adelelgo <adelelgo@hotmail.com>
2023-03-04 20:48:13 +0300

    2

--- data10.js
+++ data10.js
@@ -32,10 +32,49 @@ const getPersonById = (id) => {
   //   console.log(getData);
 
   if (itemNeeded) {
+    // calculateSum(itemNeeded.materials)
     console.log(itemNeeded);
   } else {
     console.log("No Data");
   }
+};
+
+const addAdditionalFields = (arr) => {
+  arr.forEach((element) => {
+    element.total = 0;
+    element.avg = 0;
+  });
+
+  // data111.materials.array.forEach((element) => {
+  //   element.total = 0;
+  // });
+  // calculateSum(element.materials);
+  return arr;
+};
+
+const calculateSumAndAvg = (arr) => {
+  try {
+    let total1 = 0;
+    let x = 0;
+    arr.forEach((element) => {
+      total1 = 0;
+      x = 0;
+      element.materials.forEach((material_single) => {
+        total1 += material_single.grade;
+        x += 1;
+      });
+      element.total = total1;
+      element.avg = total1 / x;
+    });
+    return arr;
+  } catch {
+    console.log("Error In calculateSum ");
+    return arr;
+  }
+
+  // return arr.reduce((total, current) => {
+  //   return total.total + current.total;
+  // }, 0);
 };
 
 const delPerson = (id) => {
@@ -58,7 +97,12 @@ const getList = () => {
 const loadData = () => {
   try {
     const dataJson = fs.readFileSync("data10.json").toString();
-    return JSON.parse(dataJson);
+
+    const dataObj = addAdditionalFields(JSON.parse(dataJson));
+
+    const dataObj2 = calculateSumAndAvg(dataObj);
+
+    return dataObj2;
   } catch {
     return [];
   }
